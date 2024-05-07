11fba713393fb3389cb63da6f665f8ed41f352e3
Bill958 <kabogobill4@gmail.com>
2024-05-07 19:26:08 +0300

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import requests
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+    
+    Args:
+        subreddit (str): The name of the subreddit.
+    """
+    # URL for the subreddit's hot posts
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Advanced API v 6/bill958'}
+
+    # Send a GET request to the Reddit API
+    response = requests.get(url, headers=headers,allow_redirects=False)
+
+    # Check if the response is successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        # Extract and print titles of the first 10 posts
+        for post in data['data']['children'][:10]:
+            print(post['data']['title'])
+    else:
+        # If the subreddit is invalid or any other error occurs, print None
+        print(None)
+
